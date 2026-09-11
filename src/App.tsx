83e2704ac5745bc242e2ad47881d336aa1006b97
Rashedulman <rashedulalam2k02@gmail.com>
2026-09-11 10:17:31 +0600

    added navbar section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Nav from './Components/Nav'
 
 function App() {
 
   return (
     <>
-     
+     <Nav/>
     </>
   )
 }
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,25 @@
+import Logo from '../assets/logo-text.png'
+const Nav = () => {
+    return (
+        <nav>
+        <div className='flex justify-between items-center container mx-auto'>
+            <img src={Logo} alt="" />
+            <ul className='flex gap-4 text-[#475569] text-md'>
+                <li><a href="">Home</a></li>
+                <li><a href="">Technologies</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div>
+                <button className='btn rounded-3xl text-[#475569]'>Sign In</button>
+                <button className='btn bg-[#D91B7E] text-white rounded-3xl'>Sign Up</button>
+            </div>
+            
+        </div>
+
+        </nav>
+    );
+};
+
+export default Nav;
